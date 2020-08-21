c1af951766f4d34f302a95ce8864f2aabb5aa65c
stevexenios <27645504+stevexenios@users.noreply.github.com>
2020-08-20 23:04:43 -0700

    p6
    init_vids

---
+++ src/components/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import youtube from '../apis/Youtube';
+import VideoDetail from './VideoDetail';
+
+
+const KEY = 'AIzaSyAAOVmhp-gyxZioHaaVhCQkVl9tk9FLYV0';
+
+class App extends React.Component{
+    state = {videos: [], selectedVideo: null};
+    
+    componentDidMount(){
+        this.onTermSubmit('business motivation');
+    }
+
+    onTermSubmit = async term => {
+        const response = await youtube.get('/search', {
+            params: {
+                q: term,
+                part: 'snippet',
+                maxResults: 5,
+                type: 'video',
+                key: KEY
+            }
+        });
+        
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
+    };
+
+    onVideoSelect = (video) => {
+        this.setState({ selectedVideo: video });
+    };
+
+    render() {
+        return (
+            <div className ="ui container">
+                <SearchBar onFormSubmit={this.onTermSubmit}/>
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video = {this.state.selectedVideo}/>
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos = {this.state.videos}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default App;
