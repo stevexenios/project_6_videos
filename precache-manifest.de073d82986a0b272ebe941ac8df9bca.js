1178a8185de465581d0f8c1c94c4c4b5ad1c4d51
xenios@uw.edu <27645504+stevexenios@users.noreply.github.com>
2020-08-20 23:34:54 -0700

    Updates

---
+++ precache-manifest.de073d82986a0b272ebe941ac8df9bca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42d33e42b1b29b00f13d38f98db5422c",
+    "url": "/project_6_videos/index.html"
+  },
+  {
+    "revision": "9ebfe7c08ba5d1e91da3",
+    "url": "/project_6_videos/static/css/main.4965ec2a.chunk.css"
+  },
+  {
+    "revision": "c6d5117c96b1ffff16b4",
+    "url": "/project_6_videos/static/js/2.4743945c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project_6_videos/static/js/2.4743945c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ebfe7c08ba5d1e91da3",
+    "url": "/project_6_videos/static/js/main.b13564f8.chunk.js"
+  },
+  {
+    "revision": "909d98681968c3714ff6",
+    "url": "/project_6_videos/static/js/runtime-main.2017e497.js"
+  }
+]);
